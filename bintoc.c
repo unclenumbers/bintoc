9ba973206339dea6c0d633727bd9b9dd28fa8d13
unclenumbers <dhk.man2500@gmail.com>
2021-11-05 17:45:52 -0400

    function prototypes added

--- bintoc.c
+++ bintoc.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
+#include <string.h>
+
+// Returns 0 on success.
+int openCFile( char *sourceName, FILE **dest );
+
+// Returns 0 on success.
+int writeData( FILE *source, FILE *dest );
 
 int main( int argc, char **argv ) {
-    printf( "Hi\n" );
+
+    if ( argc != 2 ) {
+        fprintf( stderr,
+            "usage: bintoc /path/to/binary\n"
+            "bintoc will translate the binary to a .c file\n"
+            "that has the binary as an unsinged char array named\n"
+            "ARRAY.\n" );
+        return 1;
+    }
+    
+    FILE
+        *source = fopen( argv[1], "rb" ),
+        *dest = NULL;
+
+    if ( source == NULL ) {
+        fprintf( stderr, "Source file could not be opened!\n" );
+        return 1;
+    }
+
+    if ( openCFile( argv[1], &dest ) ) {
+        fprintf( stderr, "Destination file could not be created!\n" );
+        return 1;
+    }
+    
+    if ( writeData( source, dest ) ) {
+        fprintf( stderr, "Data could not be copied!\n" );
+    }
+
+    return 0;
+}
+
+int openCFile( char *sourceName, FILE **dest ) {
+    return 0;
+}
+
+int writeData( FILE *source, FILE *dest ) {
     return 0;
 }
